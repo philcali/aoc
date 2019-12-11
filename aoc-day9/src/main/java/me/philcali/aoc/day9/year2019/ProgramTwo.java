79ed3f1e3dc1844a11d19f212d3d4e78a794b958
Philip Cali <philip.cali@gmail.com>
2019-12-10 21:48:18 -0500

    Catching up.

---
+++ aoc-day9/src/main/java/me/philcali/aoc/day9/year2019/ProgramTwo.java
@@ -0,0 +1,28 @@
+package me.philcali.aoc.day9.year2019;
+
+import com.google.auto.service.AutoService;
+
+import me.philcali.aoc.common.AnnotatedDailyEvent;
+import me.philcali.aoc.common.DailyEvent;
+import me.philcali.aoc.common.DailyInputEvent;
+import me.philcali.aoc.common.Day;
+import me.philcali.aoc.common.Problem;
+import me.philcali.aoc.common.Year;
+import me.philcali.aoc.common.intcode.Program;
+import me.philcali.aoc.common.intcode.ProgramHaltsException;
+
+@Year(2019) @Day(9) @Problem(2)
+@AutoService(DailyEvent.class)
+public class ProgramTwo implements AnnotatedDailyEvent, DailyInputEvent {
+    @Override
+    public void run() {
+        final Program program = Program.fromLines(readLines()).addInput(2);
+        try {
+            while (true) {
+                program.run();
+            }
+        } catch (ProgramHaltsException e) {
+            System.out.println("Finished");
+        }
+    }
+}
